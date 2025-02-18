696bdddd8beb4028560976d8d39ac57004607087
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-02-18 12:52:51 -0300

    Create state.js

---
+++ frontend/state.js
@@ -0,0 +1,11 @@
+/*Este arquivo mantém o estado da aplicação, ou seja,
+armazena os dados carregados da API. Sempre que os dados forem atualizados, o estado reflete essas mudanças.*/
+
+const state = {
+    pratos: [],
+    bebidas: [],
+    funcionarios: [],
+    pedidos: []
+  };
+  
+  export default state;
