dbbc224da3a5976dd34d8a963be56acb1ab2bfad
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-02-28 15:04:07 -0300

    Create carregar.js

---
+++ backend/carregar.js
@@ -0,0 +1,7 @@
+import {loadPratos, loadBebidas, loadPedidos, loadFuncionarios} from './dados.js'
+
+
+document.addEventListener('DOMContentLoaded', loadPratos);
+document.addEventListener('DOMContentLoaded', loadBebidas);
+document.addEventListener('DOMContentLoaded', loadPedidos);
+document.addEventListener('DOMContentLoaded', loadFuncionarios);
