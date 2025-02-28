2ece8b5be4c39bd43c4c102bd0af324fe3dac931
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-02-28 17:22:03 -0300

    Update PedidoRepository.js

--- backend/src/repositories/PedidoRepository.js
+++ backend/src/repositories/PedidoRepository.js
@@ -5,12 +5,12 @@ const PedidoRepository = {
 
     async findAll() {
         const rows = await connection.query("select * from pedidos", []);
-        return rows.map(row => new Pedido(row.id, row.nome));
+        return rows.map(row => new Pedido(row.id, row.ingredientes, row.preco, row.quantidade_pratos, row.quantidade_bebidas, row.clientes_id));
     },
 
     async createProject(pedido){
-        const result = await connection.query("insert into pedidos (nome) values (?)",
-            [pedido.nome]
+        const result = await connection.query("insert into pedidos (ingredientes, preco, quantidade_pratos, quantidade_bebidas, clientes_id) values (?, ?, ?, ?, ?)",
+            [pedido.ingredientes, pedido.preco, pedido.quantidade_pratos, pedido.quantidade_bebidas, pedido.clientes_id]
         );
         pedido.id = result.insertId;
         return pedido;
