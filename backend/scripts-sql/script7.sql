6f886fd796e25d971c2937ed89edbdaf3097450b
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-02-28 16:34:20 -0300

    Delete backend/scripts-sql/script7.sql

--- backend/scripts-sql/script7.sql
+++
@@ -1,8 +0,0 @@
--- Criação da Tabela pedidos
-CREATE TABLE pedido(
-    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
-    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    situação enum('pendente','em progresso', 'concluído') NOT NULL DEFAULT 'pendente',
-    id_clientes INT NOT NULL,
-    FOREIGN KEY(id_clientes) REFERENCES clientes(id_clientes)
-);
