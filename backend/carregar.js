66126499c347a928e1bedff6d1b111c9e0f2d1c0
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-02-28 16:22:33 -0300

    Delete backend/carregar.js

--- backend/carregar.js
+++
@@ -1,7 +0,0 @@
-import {loadPratos, loadBebidas, loadPedidos, loadFuncionarios} from './dados.js'
-
-
-document.addEventListener('DOMContentLoaded', loadPratos);
-document.addEventListener('DOMContentLoaded', loadBebidas);
-document.addEventListener('DOMContentLoaded', loadPedidos);
-document.addEventListener('DOMContentLoaded', loadFuncionarios);
