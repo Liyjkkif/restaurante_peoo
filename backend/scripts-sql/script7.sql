186e4e383b0c1f6e8380635e2251bfae94b1a462
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-03-05 22:39:50 -0300

    Update script7.sql

--- backend/scripts-sql/script7.sql
+++ backend/scripts-sql/script7.sql
@@ -2,7 +2,12 @@
 CREATE TABLE pedido(
     id_pedido INT AUTO_INCREMENT PRIMARY KEY,
     data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    situação enum('pendente','em progresso', 'concluído') NOT NULL DEFAULT 'pendente',
-   quantidade INT NOT NULL DEFAULT 1,
-    anotação text NOT NULL
+    comanda ENUM('01', '02', '03', '04', '05', '06', '07', '08', '09', '10') NOT NULL,
+    titular text not null default 'Desconhecido',
+    pedido text not null,
+    quantidade INT NOT NULL DEFAULT 1,
+    anotação text NOT NULL,
+    situação enum('Pendente','Em progresso', 'Concluído') NOT NULL DEFAULT 'Pendente'
 );
+
+insert into pedido (titular, comanda, pedido, quantidade, anotação) VALUES ( 'César', '01','Suco de Abacaxi com Hortelã', 1, 'Sem açúcar');
