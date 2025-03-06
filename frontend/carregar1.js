7125d6d55f666e41e1274886dba8299a734c2d41
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-03-05 22:45:23 -0300

    Update and rename carregar.js to carregar1.js

--- frontend/carregar1.js
+++ frontend/carregar1.js
@@ -1,7 +1,6 @@
-import {loadPratos, loadBebidas, loadPedidos, loadFuncionarios} from './dados.js'
+import {loadPratos, loadBebidas, loadPedidos} from './dados.js'
 
 
 document.addEventListener('DOMContentLoaded', loadPratos);
 document.addEventListener('DOMContentLoaded', loadBebidas);
 document.addEventListener('DOMContentLoaded', loadPedidos);
-document.addEventListener('DOMContentLoaded', loadFuncionarios);
