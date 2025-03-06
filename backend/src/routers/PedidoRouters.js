50d5912913201c7c4669cbbe1e95ebc8d052bea1
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-03-05 22:36:47 -0300

    Delete backend/src/routers/PedidoRouters.js

--- backend/src/routers/PedidoRouters.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import PedidoController from "../controllers/PedidoController.js";
-
-const PedidoRouters = express.Router();
-
-PedidoRouters.get("/", PedidoController.getAll);
-PedidoRouters.post("/", PedidoController.create);
-
-export default PedidoRouters;
