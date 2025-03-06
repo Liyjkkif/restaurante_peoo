6a48c93ced27980ba8b079156c5df08c3939a78c
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-03-05 22:43:50 -0300

    Update BebidaRepository.js

--- backend/src/repositories/BebidaRepository.js
+++ backend/src/repositories/BebidaRepository.js
@@ -1,20 +1,21 @@
-import Bebida from "../models/Bebida.js";
+import TabelaPedidos from "../models/TabelaPedidos.js";
 import connection from "../config/dbConnect.js";
 
-const BebidaRepository = {
+const TabelaPedidosRepository = {
 
     async findAll() {
-        const rows = await connection.query("select * from bebidas", []);
-        return rows.map(row => new Bebida(row.id, row.nome, row.ingredientes, row.preco));
+        const rows = await connection.query("select * from pedido", []);
+        return rows.map(row => new TabelaPedidos(row.id_pedido, row.data_hora, row.comanda, row.titular, row.pedido, row.quantidade,  row.anotação, row.situação));
     },
 
-    async createProject(bebida){
-        const result = await connection.query("insert into bebidas (nome, ingredientes, preco) values (?, ?, ?)",
-            [bebida.nome, bebida.ingredientes, bebida.preco]
+    async createTabelaPedido(tabelaPedido) {
+        const result = await connection.query(
+            "insert into pedido (data_hora, titular, comanda, pedido, quantidade, anotação, situação) values (?, ?, ?, ?, ?, ?, ?)",
+            [tabelaPedido.getDataHora(), tabelaPedido.getTitular(), tabelaPedido.getComanda(), tabelaPedido.getPedido(), tabelaPedido.getQuantidade(), tabelaPedido.getAnotacao(), tabelaPedido.getSituacao()]
         );
-        bebida.id = result.insertId;
-        return bebida;
+        tabelaPedido.setIdPedido(result.insertId);
+        return tabelaPedido;
     }
-}
+};
 
-export default BebidaRepository;
+export default TabelaPedidosRepository;
