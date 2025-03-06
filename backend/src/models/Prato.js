000b14ba48d8ca9aff43d59f482d144025fbfea8
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-03-05 22:42:35 -0300

    Update Prato.js

--- backend/src/models/Prato.js
+++ backend/src/models/Prato.js
@@ -1,12 +1,17 @@
 class Prato {
-    constructor(id, ingredientes, preco){
+    constructor(id, nome, ingredientes, preco){
         this._id = id;
+        this._nome = nome;
         this._ingredientes = ingredientes;
         this._preco = preco;
     }
 
     setId(id){
         this._id = id;
+    }
+
+    setNome(nome){
+        this._nome = nome;
     }
 
     setIngredientes(ingredientes){
@@ -19,6 +24,10 @@ class Prato {
 
     getId(){
         return this._id;
+    }
+
+    setNome(){
+        return this._nome;
     }
 
     getIngredientes(){
