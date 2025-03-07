e1d1e3379c2d76a96218781d4bbf10f2d20cb00f
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-03-07 16:11:34 -0300

    Update script2.sql

--- backend/scripts-sql/script2.sql
+++ backend/scripts-sql/script2.sql
@@ -1,13 +1,24 @@
--- Criar tabela funcionarios
 CREATE TABLE funcionarios(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(20) NOT NULL,
     funcao ENUM('Cozinheiro', 'Gerente', 'Garçom') NOT NULL,
+    email VARCHAR(20) NOT NULL,
     senha VARCHAR(8) NOT NULL,
     restaurantes_id INT NOT NULL,
-    FOREIGN KEY (restaurantes_id) REFERENCES restaurantes(id)
+    FOREIGN KEY(restaurantes_id) REFERENCES restaurantes(id)
 ); -- Tabela funcionarios relacionada com tabela restaurantes
-
 -- Inserir valores na tabela funcionarios
-INSERT INTO funcionarios (nome, funcao, senha, restaurantes_id)
-VALUES ('Júlio César','Gerente','33446699','1');
+INSERT INTO funcionarios(
+    nome,
+    funcao,
+    email,
+    senha,
+    restaurantes_id
+)
+VALUES(
+    'Júlio César',
+    'Gerente',
+    'juliocesar@123',
+    '33446699',
+    '1'
+);
