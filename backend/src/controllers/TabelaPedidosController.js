74aaeae1fa83397491f83ca4849c5ee30f8d335c
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-03-07 16:20:15 -0300

    Update TabelaPedidosController.js

--- backend/src/controllers/TabelaPedidosController.js
+++ backend/src/controllers/TabelaPedidosController.js
@@ -7,18 +7,18 @@ const TabelaPedidosController = {
             const tabelapedidos = await TabelaPedidosRepository.findAll();
             res.json(tabelapedidos);
         }catch(err){
-            res.status(500).json({error : "Erro na busca de registros de TabelaPedidos", err});
+            res.status(500).json({error : "Erro na busca de registros de Pedidos", err});
         }
     },
 
     async create(req, res) {
-        const {nome} = req.body;
+       const novoRegistro = req.body;
         try {
-            const novoRegistro = {nome};
             const registroCriado = await TabelaPedidosRepository.createTabelaPedido(novoRegistro);
             res.status(201).json(registroCriado);
         }catch(err){
-            res.status(500).json({error : "Erro na criação de registros de TabelaPedidos", err});
+            console.log('erro')
+            res.status(500).json({error : "Erro na criação de registros de Pedidos", err});
         }
     }
 
