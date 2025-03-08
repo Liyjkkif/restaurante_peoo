c37a8d36acdea07f89a61c6de47f8a1519ab91ce
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-03-07 21:16:16 -0300

    Update RestauranteController.js

--- backend/src/controllers/RestauranteController.js
+++ backend/src/controllers/RestauranteController.js
@@ -1,21 +1,26 @@
-import Restaurante from "../models/Restaurante.js";
-import connection from "../config/dbConnect.js";
+import RestauranteRepository from "../repositories/RestauranteRepository.js";
 
-const RestauranteRepository = {
+const RestauranteController = {
 
-    async findAll() {
-        const rows = await connection.query("select * from restaurantes", []);
-        return rows.map(row => new Restaurante(row.id, row.nome, row.endereco, row.telefone));
+    async getAll(req, res){
+        try {
+            const restaurantes = await RestauranteRepository.findAll();
+            res.json(restaurantes);
+        }catch(err){
+            res.status(500).json({error : "Erro na busca de restaurantes", err});
+        }
     },
 
-    async createProject(restaurante){
-        const result = await connection.query("insert into restaurante (nome, endereco, telefone) values (?, ?, ?)",
-            [restaurante.getNome(), restaurante.getEndereco(), restaurante.getTelefone()]
-        );
-        restaurante.getId(result.insertId);
-        return restaurante;
+    async create(req, res) {
+        const novoRestaurante = req.body;
+        try {
+            const restauranteCriado = await RestauranteRepository.createProject(novoRestaurante);
+            res.status(201).json(restauranteCriado);
+        }catch(err){
+            res.status(500).json({error : "Erro na criação de restaurantes", err});
+        }
     }
+
 }
 
-export default RestauranteRepository;
-
+export default RestauranteController;
